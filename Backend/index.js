37d07ea5663a0de2fe467e8825dc9d07d4f09f67
Akin <akinalan15@gmail.com>
2024-11-13 00:28:02 +0530

    img

--- Backend/index.js
+++ Backend/index.js
@@ -47,7 +47,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
+    image_url: `https://e-commerse-1-qck4.onrender.com/images/${req.file.filename}`,
   });
 });
 
